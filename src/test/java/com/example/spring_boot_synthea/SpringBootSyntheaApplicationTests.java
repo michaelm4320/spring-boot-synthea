3eb16934078d608f947c4167230a2cdd9f9332df
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-02 19:41:45 -0400

    initial commit

---
+++ src/test/java/com/example/spring_boot_synthea/SpringBootSyntheaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_synthea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSyntheaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
