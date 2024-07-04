f06b81e3ee9b234eadfa18317f784d346e9f55fc
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-03 20:22:13 -0400

    test spring boot controller

---
+++ src/main/java/com/example/spring_boot_synthea/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_synthea;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
--- src/main/java/com/example/spring_boot_synthea/SpringBootSyntheaApplication.java
+++ src/main/java/com/example/spring_boot_synthea/SpringBootSyntheaApplication.java
@@ -1,7 +1,13 @@
 package com.example.spring_boot_synthea;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootSyntheaApplication {
@@ -10,4 +16,17 @@ public class SpringBootSyntheaApplication {
 		SpringApplication.run(SpringBootSyntheaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
