be4af9bdd841ad57fa3202c1b77867fff6353238
Michael Martinez <michaelmartinez4320@yahoo.com>
2024-07-06 23:48:46 -0400

    connect database with test name

--- src/main/java/com/example/spring_boot_synthea/SpringBootSyntheaApplication.java
+++ src/main/java/com/example/spring_boot_synthea/SpringBootSyntheaApplication.java
@@ -1,12 +1,16 @@
 package com.example.spring_boot_synthea;
 
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -19,14 +23,35 @@ public class SpringBootSyntheaApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
 				System.out.println(beanName);
 			}
+
+			// Call the method to connect to the database
+			connectToDatabase();
 		};
+	}
+
+	private void connectToDatabase() {
+		try {
+			String password = System.getenv("DB_PASSWORD");
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1:3306/synthea",
+					"root",
+					password
+			);
+
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM NAMES");
+
+			while (resultSet.next()) {
+				System.out.println(resultSet.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
